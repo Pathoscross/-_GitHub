2f022f2c23268589c906ba9f34ad564f804124f1
Pathoscross <pathoscross@gmail.com>
2015-11-21 12:09:06 +0800

    20151121

---
+++ C#_HOMEWORK/PrimeNumber/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] fibarray = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 15 };
+            foreach (int element in fibarray)
+            {
+                System.Console.WriteLine(element);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ C#_HOMEWORK/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int beginning;
+            int final;
+            string enterNumber;
+
+            Console.WriteLine("尋找質數");
+            Console.Write("請輸入起始數字：");
+            enterNumber = Console.ReadLine();
+            beginning = prise(enterNumber);
+
+            Console.Write("請輸入結束數字：");
+            enterNumber = Console.ReadLine();
+            final = prise(enterNumber);
+
+            primeCheck(beginning, final);
+        }
+
+        private static int prise(string number)
+        {
+            int correctNumber = 0;
+            bool tryPrise;
+
+            do
+            {
+                tryPrise = int.TryParse(number, out correctNumber);
+
+                if (tryPrise == false)
+                {
+                    Console.WriteLine("輸入錯誤，請輸入重新輸入");
+                    number = Console.ReadLine();
+                }
+            } while (tryPrise == false);
+
+            return correctNumber;
+        }
+
+        private static void primeCheck(int min, int max)
+        {
+            bool isPrime = true;
+
+            int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17 };
+        }
+    }
+}
